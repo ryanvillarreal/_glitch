32beb343c3198948a7d0834d2f66e45e2cd436c0
xjcrazy <ryan@villarreal.com>
2026-02-17 12:52:02 -0500

    merging base into mini for easier debugging

--- hosts/mini/default.nix
+++ hosts/mini/default.nix
@@ -6,9 +6,64 @@
 }:
 {
   # build file system first and foremost
-  imports = [ 
-    ../base/default.nix
+  # imports = [ 
+  #   ../base/default.nix
+  # ];
+  # This fixes the "experimental feature" error
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
   ];
+
+  # This fixes the <nixpkgs> error by linking the system's nixpkgs
+  # to the legacy channels path
+  nix.nixPath = [ "nixpkgs=${pkgs.path}" ];
+
+  # build file system first and foremost
+  imports = [ ../common/disko.nix ];
+
+  # personal preference cause i'm lazy
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowBroken = true;
+  };
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # security configs
+  security.rtkit.enable = true;
+  security.sudo-rs.enable = true;
+  system.autoUpgrade.enable = true;
+
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.initrd.supportedFilesystems = [ "zfs" ];
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # finally, read the manual before editing
+  system.stateVersion = "25.11";
 
   # network should be uniq for each machine
   networking = {
@@ -47,7 +102,7 @@
 
   users.users.xj = {
     isNormalUser = true;
-    initialPassword = "xj"; # force change on first login
+    initialPassword = "xj"; # force change on first login?
     home = "/home/xj";
     description = "xj";
     extraGroups = [
